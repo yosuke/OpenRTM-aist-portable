ac0ab24f1cf9e62cac52ccc68f2fc55e4f6b75a6
Yosuke Matsusaka <yosuke.matsusaka@aist.go.jp>
2011-12-19 16:09:03 +0900

    original openrtm-aist trunk

---
+++ src/lib/rtm/DataInPort.h
@@ -0,0 +1,25 @@
+// -*- C++ -*-
+/*!
+ * @file DataInPort.h
+ * @brief Base class of InPort
+ * @date $Date: 2008-01-13 15:06:58 $
+ * @author Noriaki Ando <n-ando@aist.go.jp>
+ *
+ * Copyright (C) 2006-2009
+ *     Noriaki Ando
+ *     Task-intelligence Research Group,
+ *     Intelligent Systems Research Institute,
+ *     National Institute of
+ *         Advanced Industrial Science and Technology (AIST), Japan
+ *     All rights reserved.
+ *
+ * $Id: DataInPort.h 1326 2009-05-18 00:47:29Z n-ando $
+ *
+ */
+
+#ifndef RTC_DATAINPORT_H
+#define RTC_DATAINPORT_H
+
+#include <rtm/InPort.h>
+
+#endif // RTC_DATAINPORT_H
